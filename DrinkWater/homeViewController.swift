e739d3ecbfa295622f3b07503cd21376525b3cda
dh2487 <davidhdoev@gmail.com>
2019-07-16 20:50:15 -0700

    Moved token to firebase

--- DrinkWater/homeViewController.swift
+++ DrinkWater/homeViewController.swift
@@ -13,7 +13,7 @@ import MessageUI
 import FirebaseDatabase
 import Alamofire
 
-class homeViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+class homeViewController: UIViewController {
 
     @IBOutlet weak var sendItButton: UIButton!
     @IBOutlet weak var contactToSendTo: UILabel!
@@ -60,44 +60,34 @@ class homeViewController: UIViewController, MFMessageComposeViewControllerDelega
         super.didReceiveMemoryWarning()
     }
     
-    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
-        sendMessage()
-    }
     
     @objc private func sendMessage() {
         let Messages = waterMessages()
         let message = Messages.messages[Int(arc4random()) % Messages.messages.count]
         
         print (message)
-        
         let accountSID = "AC72ce0e1e8873389e9467edfd9eabd86e"
-        let authToken = "515e1ee53415c2619f8f2290c92d9ba8"
-        let url = "https://api.twilio.com/2010-04-01/Accounts/\(accountSID)/Messages"
-        let parameters = ["From": "+12133194018", "To": "+12136055210", "Body": "Drink Water Slut test!"]
-            
-        Alamofire.request(url, method: .post, parameters: parameters)
-            .authenticate(user: accountSID, password: authToken)
-            .responseJSON { response in
-                debugPrint(response)
-                print("RESPONSE", response)
-        }
-
-        let reference = Database.database().reference()
-        reference.child(UIDevice.current.identifierForVendor!.uuidString).child("Contact").updateChildValues(["Phone Number" : message])
         
-//        let messageViewController = MFMessageComposeViewController()
-//        messageViewController.messageComposeDelegate = self
-//
-//        // Configure the fields of the interface.
-//        messageViewController.recipients = ["2136055210"]
-//        messageViewController.body = "Drink Water you Slut!"
-//
-//        // Present the view controller modally.
-//        if MFMessageComposeViewController.canSendText() {
-//            self.present(messageViewController, animated: true, completion: nil)
-//        } else {
-//            print("Can't send messages.")
-//        }
+        let _ = Database.database().reference().child("key").observe(.value) { (snapshot) in
+            if let value = snapshot.value as? String {
+                let authToken = value
+                let url = "https://api.twilio.com/2010-04-01/Accounts/\(accountSID)/Messages"
+                let parameters = ["From": "+12133194018", "To": "+12136055210", "Body": message]
+                
+                Alamofire.request(url, method: .post, parameters: parameters)
+                    .authenticate(user: accountSID, password: authToken)
+                    .responseJSON { response in
+                        debugPrint(response)
+                        print("RESPONSE", response)
+                }
+                
+                let reference = Database.database().reference()
+                reference.child(UIDevice.current.identifierForVendor!.uuidString).child("Contact").updateChildValues(["Phone Number" : message])
+            }
+            else {
+                return
+            }
+        }
     }
     
     private func fetchContacts() {
@@ -122,17 +112,10 @@ class homeViewController: UIViewController, MFMessageComposeViewControllerDelega
         }
     }
 
-//    @IBAction func drinkWaterButtonPressed(_ sender: Any) {
-//        print("Pressed")
-//        sendMessage()
-//    }
-//
-//    @IBAction func settingsButtonPressed(_ sender: Any) {
-//        print("Settings")
-//    }
-    
-    
-    
+    @IBAction func drinkWaterButtonPressed(_ sender: Any) {
+        print("Pressed")
+        sendMessage()
+    }
 }
 
 extension homeViewController: UITableViewDataSource, UITableViewDelegate {
